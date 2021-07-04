e6b057f400f17f5d082f43fc2e68afc013e69d8c
Yameen <yaamynu@gmail.com>
2021-07-04 20:31:52 +0500

    add rejection sms template

---
+++ resources/views/sms/rejection.blade.php
@@ -0,0 +1,5 @@
+The vaccine appointment for {{$booking->name}} ({{$booking->identifier}}) could not be confirmed. 
+If you are entitled to get a vaccine dose please use this link to raise a ticket with us, we will reach you asap. 
+
+Thank you
+HPA
