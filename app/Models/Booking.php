3be794c080fa85b31c267aac150d0fcd2261aae4
Yameen <yaamynu@gmail.com>
2021-07-05 14:41:50 +0500

    make cancellable only if not rejected

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -50,7 +50,7 @@ class Booking extends Model
 
     public function getIsCancellableAttribute()
     {
-        return true; // $this->status == 'pending' && Carbon::parse($this->slot->date.' '.$this->slot->start)->gt(now());
+        return $this->status != 'rejected'; // $this->status == 'pending' && Carbon::parse($this->slot->date.' '.$this->slot->start)->gt(now());
     }
 
 }
